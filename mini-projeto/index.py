1bdc41a32c955bebf5206c2a4e2963807b95a7b3
velvix321 <velvix321@gmail.com>
2025-07-04 11:18:39 -0300

    commit

--- mini-projeto/index.py
+++ mini-projeto/index.py
@@ -1,36 +1,28 @@
-class  banco_de_dados():
+class  bancoDeDados():
     def __init__(self, nome, idade, numero):
         self._nome = nome
         self._idade = idade
         self._numero = numero
     
-class usuario(banco_de_dados):
+    
+class usuario(bancoDeDados):
     def __init__(self, nome, idade, numero):
         super().__init__(nome, idade, numero)
     
-    @property
     def mostra_usuario(self):
         print(f"{self._nome}, {self._idade}, {self._numero}")
 
-class admin(banco_de_dados):
+
+class admin(bancoDeDados):
     def __init__(self, nome, idade, numero):
         super().__init__(nome, idade, numero)
     
-    @property
-    def idade(self):
-        return self._idade
+    def mostra_tudo(self):
+        print(f"{self._nome}, {self._idade}, {self._numero}")
     
     @property
     def nome(self):
         return self._nome  
-    
-    @property
-    def numero(self):
-        return self._numero
-    
-    @property
-    def mostra_tudo(self):
-        print(f"{self._nome}, {self._idade}, {self._numero}")
     
     @nome.deletar
     def nome(self):
@@ -40,6 +32,11 @@ class admin(banco_de_dados):
         else:
             print("nome ja foi deletado")
 
+
+    @property
+    def idade(self):
+        return self._idade
+
     @idade.deletar
     def idade(self):
         if hasattr(self, "_idade"):
@@ -47,6 +44,11 @@ class admin(banco_de_dados):
             del self._idade
         else:
             print("idade ja foi deletado.")
+    
+
+    @property
+    def numero(self):
+        return self._numero
     
     @numero.deletar
     def numero(self):
